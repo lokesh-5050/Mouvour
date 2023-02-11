06cade77c712b1203bc71b5ea28e673ae94349e1
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2023-02-11 23:47:48 +0530

    Update SharedHomePage.jsx

--- src/Pages/SharedHomePage.jsx
+++ src/Pages/SharedHomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Components/Navbar/Navbar'
+//have to add infinite scroll
 const SharedHomePage = () => {
   return (
     <>
